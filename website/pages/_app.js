60aa805697e036ed59e73f979c3718f039f158f6
lenny-of-the-cool-waters <lennygith@gmail.com>
2022-02-09 16:29:28 +0300

    Login page

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CookiesProvider } from 'react-cookie';
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 
 import createEmotionCache from '../utility/createEmotionCache';
-import lightTheme from '../styles/theme/lightTheme';
+import themes from '../styles/themes/index.js';
 import '../styles/globals.css';
 
 const clientSideEmotionCache = createEmotionCache();
@@ -13,12 +14,14 @@ const MyApp = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <CookiesProvider>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={themes.default}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </CookiesProvider>
   );
 };
 
---
+++ website/pages/login/ResetPassword.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import {
+  Grid,
+  CircularProgress,
+  Typography,
+  Button,
+  Tabs,
+  Tab,
+  TextField,
+  Fade,
+} from '@mui/material';
+
+import useStyles from "./styles";
+
+// import auth context and functions
+
+export const ResetPassword = (props) => {
+  const classes = useStyles();
+  // let userDispatch = useUserDispatch();
+
+  let [isLoading, setIsLoading] = useState(false);
+  let [error, setError] = useState(false);
+  let [errorMessage, setErrorMessage] = useState("");
+  let [status, setStatus] = useState(false);
+  let [message, setMessage] = useState("");
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
+  let [confirmPassword, setConfirmPassword] = useState("");
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return (
+    <React.Fragment>
+      <Typography variant="h1" className={classes.greeting}>
+        Reset Password
+      </Typography>
+
+      <Fade in={error}>
+        <Typography color="primary" className={classes.errorMessage}>
+          {errorMessage}
+        </Typography>
+      </Fade>
+      <Fade in={status}>
+        <Typography color="secondary" className={classes.errorMessage}>
+          {message}
+        </Typography>
+      </Fade>
+      <TextField
+        id="username"
+        InputProps={{
+          classes: {
+            underline: classes.textFieldUnderline,
+            input: classes.textField,
+          },
+        }}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        margin="normal"
+        placeholder="Username"
+        fullWidth
+        required
+      />
+      <div className={classes.formButtons}>
+        {isLoading ? (
+          <CircularProgress size={26} className={classes.loginLoader} />
+        ) : (
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            disabled={username.length === 0}
+            onClick={() => {
+              console.log("reseting the password")
+            }}
+          >
+            Send Reset Link
+          </Button>
+        )}
+      </div>
+    </React.Fragment>
+  );
+};
